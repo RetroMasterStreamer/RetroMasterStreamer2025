ede3aaa03c29e7d133fcf4024404036dbb37bc61
fmateluna <fernando.mateluna@gmail.com>
2024-05-06 09:42:48 -0400

    Pruebas de deploy 2

--- main.go
+++ main.go
@@ -10,7 +10,11 @@ func main() {
 		fmt.Println("Buscando index!!")
 		http.ServeFile(w, r, "static/index.html")
 	})
+	http.HandleFunc("/saludo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Buscando index!!")
+		fmt.Fprintf(w, "TODO OK!!")
 
+	})
 	fmt.Println("Servidor escuchando en el puerto :80, sin novedades")
 	http.ListenAndServe(":80", nil)
 }
